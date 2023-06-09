11028e726f5b678caa7194454d8be073e26127e9
Nick Sacco <nicksacco10@gmail.com>
2023-06-08 17:22:18 -0700

    test: coupon model relationship tests

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -2,10 +2,11 @@ require "rails_helper"
 
 RSpec.describe Coupon, type: :model do
   describe "relationships" do
-
+    it { should belong_to :merchant }
+    it { has_one :invoice }
   end
 
   describe "validations" do
-    
+
   end
 end
