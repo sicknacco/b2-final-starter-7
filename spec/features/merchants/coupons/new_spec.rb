d08e883974941ac54cc21e317384d6051c453ea6
Nick Sacco <nicksacco10@gmail.com>
2023-06-10 10:27:16 -0700

    test: fill in new coupon form with happy path

--- spec/features/merchants/coupons/new_spec.rb
+++ spec/features/merchants/coupons/new_spec.rb
@@ -3,20 +3,31 @@ require 'rails_helper'
 RSpec.describe "New Coupon Form", type: :feature do
   before(:each) do
     @merch = Merchant.create!(name: "Jim Bob's")
-    @coupon1 = @merch.coupons.create!(name: "$10 off", code: "OFF10", value: 10.00, activated: false)
-    @coupon2 = @merch.coupons.create!(name: "10% off", code: "TAKE10PER", value: 0.10, activated: false)
+    visit(new_merchant_coupon_path(@merch))
   end
 
   describe "Creating a new coupon for a merchant" do
     describe "has a form to create a new coupon"
     it "can confirm form elements" do
-      visit(new_merchant_coupon_path(@merch))
-
       expect(page).to have_content("Create New Coupon")
       expect(page).to have_field("Name")
       expect(page).to have_field("Code")
       expect(page).to have_field("Value")
       expect(page).to have_field("Value type")
+    end
+    
+    it "can fill in the new coupon form and confirm creation" do
+      fill_in "Name", with: "Spring Sale"
+      fill_in "Code", with: "SPRING50"
+      fill_in "Value", with: 0.50
+      select "percent", from: "Value type"
+      click_button("Create Coupon")
+
+      expect(current_path).to eq(merchant_coupons_path(@merch))
+      expect(page).to have_content("Coupon successfully created!")
+      expect(page).to have_content("Spring Sale")
+      expect(page).to have_content(0.50)
+      expect(page).to have_content("percent")
     end
   end
 end
