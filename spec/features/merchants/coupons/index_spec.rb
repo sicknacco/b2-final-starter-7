6508859b4c9086d085e4c7e714c0e73c2c735f14
Nick Sacco <nicksacco10@gmail.com>
2023-06-10 08:48:45 -0700

    test: new coupon link on index tests

--- spec/features/merchants/coupons/index_spec.rb
+++ spec/features/merchants/coupons/index_spec.rb
@@ -33,13 +33,29 @@ RSpec.describe "Merchant's coupons index page", type: :feature do
       
       it "each name is a link that leads to the coupon show page" do
         visit(merchant_coupons_path(@merch))
-
+        
         within "#coupon_#{@coupon1.id}" do
           expect(page).to have_link(@coupon1.name)
           click_link(@coupon1.name)
         end
         expect(current_path).to eq(merchant_coupon_path(@merch, @coupon1))
         expect(page).to have_content(@coupon1.name)
+      end
+    end
+    
+    describe "New Merchant Coupon Link" do
+      describe "merchant coupon index page has link to create a new coupon" do
+        it "can click a link that leads to the new coupon form" do
+          visit(merchant_coupons_path(@merch))
+
+          within "#new_coupon" do
+            expect(page).to have_link("Create New Coupon")
+
+            click_link("Create New Coupon")
+
+            expect(current_path).to eq(new_merchant_coupon_path(@merch))
+          end
+        end
       end
     end
   end
