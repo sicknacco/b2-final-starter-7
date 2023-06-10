c1e19a8bbfccfafab3b17d8045fe918a501e9413
Nick Sacco <nicksacco10@gmail.com>
2023-06-10 07:31:47 -0700

    feat: fix model relationships coupons added to merchants

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,5 +2,5 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   # has_many :items, through: :merchant
   # has_many :invoice_items, through: :items
-  has_many :invoice
+  has_many :invoices
 end
