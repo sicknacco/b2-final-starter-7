adea95ab6c8fb456fd0383bb25954fa0694305f1
Nick Sacco <nicksacco10@gmail.com>
2023-06-10 10:37:16 -0700

    debug to expected error

--- spec/features/merchants/coupons/new_spec.rb
+++ spec/features/merchants/coupons/new_spec.rb
@@ -10,19 +10,22 @@ RSpec.describe "New Coupon Form", type: :feature do
     describe "has a form to create a new coupon"
     it "can confirm form elements" do
       expect(page).to have_content("Create New Coupon")
-      expect(page).to have_field("Name")
-      expect(page).to have_field("Code")
-      expect(page).to have_field("Value")
-      expect(page).to have_field("Value type")
+      within "#new_coupon_form" do
+        expect(page).to have_field("Name")
+        expect(page).to have_field("Code")
+        expect(page).to have_field("Value")
+        expect(page).to have_field("Value type")
+      end
     end
     
     it "can fill in the new coupon form and confirm creation" do
-      fill_in "Name", with: "Spring Sale"
-      fill_in "Code", with: "SPRING50"
-      fill_in "Value", with: 0.50
-      select "percent", from: "Value type"
-      click_button("Create Coupon")
-
+      within "#new_coupon_form" do
+        fill_in "Name", with: "Spring Sale"
+        fill_in "Code", with: "SPRING50"
+        fill_in "Value", with: 0.50
+        select "percent", from: "Value type"
+        click_button("Create Coupon")
+      end
       expect(current_path).to eq(merchant_coupons_path(@merch))
       expect(page).to have_content("Coupon successfully created!")
       expect(page).to have_content("Spring Sale")
