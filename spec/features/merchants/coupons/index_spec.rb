9e5550248221e74e78f69019a422bb75d843ea79
Nick Sacco <nicksacco10@gmail.com>
2023-06-10 06:39:12 -0700

    test: merchant coupon index page tests

--- spec/features/merchants/coupons/index_spec.rb
+++ spec/features/merchants/coupons/index_spec.rb
@@ -9,12 +9,35 @@ RSpec.describe "Merchant's coupons index page", type: :feature do
 
   describe "When visiting a merchant's coupon index page" do
     describe "I see all coupon names and their amount off" do
-      it "can display name and the amount off for each" do
+      it "displays coupon name and amount off" do
+        visit(merchant_coupons_path(@merchant1))
 
-      end
-
-      it "each coupon name is a link that leads to its show page" do
+        within "#coupon_#{@coupon1.id}" do
+          expect(page).to have_link(@coupon1.name)
+          expect(page).to have_content("Coupon Name: #{@coupon1.name}")
+          expect(page).to have_content("Coupon Amount Off: #{@coupon1.value}")
+          expect(page).to_not have_link(@coupon2.name)
+          expect(page).to_not have_content("Coupon Amount Off: #{@coupon2.value}")
+          expect(page).to_not have_content("Coupon Name: #{@coupon2.name}")
+        end
         
+        within "#coupon_#{@coupon2.id}" do
+          expect(page).to have_link(@coupon2.name)
+          expect(page).to have_content("Coupon Name: #{@coupon2.name}")
+          expect(page).to have_content("Coupon Amount Off: #{@coupon2.value}")
+          expect(page).to_not have_link(@coupon1.name)
+          expect(page).to_not have_content("Coupon Amount Off: #{@coupon1.value}")
+          expect(page).to_not have_content("Coupon Name: #{@coupon1.name}")
+        end
+      end
+      
+      xit "each name is a link that leads to the coupon show page" do
+        within "#coupon_#{@coupon1.id}" do
+          expect(page).to have_link(@coupon1.name)
+
+          click_link(@coupon1.name)
+          expect(current_path).to eq(merchant_coupon_path(@merc, @coupon1))
+        end
       end
     end
   end
