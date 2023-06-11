26099405101a1e3cbcf6ca5858dee46dc8e7f0a6
Nick Sacco <nicksacco10@gmail.com>
2023-06-11 09:04:19 -0700

    change coupon default status to true

--- db/migrate/20230608223131_create_coupons.rb
+++ db/migrate/20230608223131_create_coupons.rb
@@ -5,7 +5,7 @@ class CreateCoupons < ActiveRecord::Migration[7.0]
       t.string :code, unique: true
       t.decimal :value
       t.references :merchant, null: false, foreign_key: true
-      t.boolean :activated, default: false
+      t.boolean :activated, default: true
 
       t.timestamps
     end
