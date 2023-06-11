7027fae60188577e8c60c9d48927d5f2a739ba86
Nick Sacco <nicksacco10@gmail.com>
2023-06-11 10:40:09 -0700

    test: deactivate coupon button tests

--- spec/features/coupons/show_spec.rb
+++ spec/features/coupons/show_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Coupon's Show Page", type: :feature do
-  before(:each) do
-    @merch = Merchant.create!(name: "Jim Bob's")
-    @coupon1 = @merch.coupons.create!(name: "$10 off", code: "OFF10", value: 10.00, value_type: 1, activated: false)
-    @coupon2 = @merch.coupons.create!(name: "10% off", code: "TAKE10PER", value: 0.10, value_type: 0, activated: false)
-  end
-
+  
   describe "Merchant Coupon Index page links to show" do
     it "each name is a link that leads to that coupon's show page" do
+      @merch = Merchant.create!(name: "Jim Bob's")
+      @coupon1 = @merch.coupons.create!(name: "$10 off", code: "OFF10", value: 10.00, value_type: 1, activated: false)
+      @coupon2 = @merch.coupons.create!(name: "10% off", code: "TAKE10PER", value: 0.10, value_type: 0, activated: false)
       visit(merchant_coupons_path(@merch))
 
       within "#coupon_#{@coupon1.id}" do
@@ -34,14 +32,29 @@ RSpec.describe "Coupon's Show Page", type: :feature do
   describe "Coupon show page details" do
     it "shows coupon name, code, value, status, and number of times it's been used" do
       test_data
-      visit(merchant_coupon_path(@merch, @coupon2))
+      visit(merchant_coupon_path(@merchant1, @coupon1))
+      
+      expect(page).to have_content("#{@coupon1.name} Show Page")
+      expect(page).to have_content("Code: #{@coupon1.code}")
+      expect(page).to have_content("Value: #{@coupon1.value}")
+      expect(page).to have_content("Value Type: #{@coupon1.value_type}")
+      expect(page).to have_content("Coupon Activated?: #{@coupon1.activated}")
+      expect(page).to have_content("Times Used: #{@coupon1.times_used}")
+    end
+  end
+  
+  describe "Deactivate coupon button" do
+    it "has a button to deactivate a coupon" do
+      test_data
+      visit(merchant_coupon_path(@merchant1, @coupon1))
 
-      expect(page).to have_content("#{@coupon2.name} Show Page")
-      expect(page).to have_content("Code: #{@coupon2.code}")
-      expect(page).to have_content("Value: #{@coupon2.value}")
-      expect(page).to have_content("Value Type: #{@coupon2.value_type}")
-      expect(page).to have_content("Coupon Activated?: #{@coupon2.activated}")
-      expect(page).to have_content("Times Used: #{@coupon2.times_used}")
+      expect(page).to have_content("Coupon Activated?: true")
+      expect(page).to have_button("Deactivate #{@coupon1.name}")
+
+      click_button("Deactivate #{@coupon1.name}")
+
+      expect(current_path).to eq(merchant_coupon_path(@merchant1, @coupon1))
+      expect(page).to have_content("Coupon Activated?: false")
     end
   end
 end
